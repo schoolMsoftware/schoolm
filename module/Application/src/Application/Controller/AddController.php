5344884e4bc91ac127de5290daf1da208779c225
ravi ranjan <raviducat@gmail.com>
2015-07-11 10:48:47 +0530

    comminting whole project skelton

---
+++ module/Application/src/Application/Controller/AddController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class AddController extends AbstractActionController
+{
+    public function indexAction()
+    {
+		//$this->layout('layout/layout2');
+        return new ViewModel();
+    }
+	
+	public function addAction(){
+		return new ViewModel();
+	}
+}
---
+++ module/Application/src/Application/Model/UserTable.php
@@ -0,0 +1,14 @@
+<?php
+namespace Application\Model;
+use Zend\Db\Sql\Sql;
+class UserTable{
+	public function xyz(){
+		//$sm = $this->getServiceLocator();
+		//print_r($sm);
+		echo "xyz";
+	}
+	
+	public function fetchData(){
+		
+	}
+}

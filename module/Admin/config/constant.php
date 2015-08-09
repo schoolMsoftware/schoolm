990ae61838f6bce2ac09917b31bf3d49018af64e
ravi ranjan <raviducat@gmail.com>
2015-08-09 15:43:22 +0530

    constant file commited

--- module/Admin/config/constant.php
+++ module/Admin/config/constant.php
@@ -1,3 +1,3 @@
 <?php
-$GLOBALS['SITE_ADMIN_URL'] = 'http://' .$_SERVER['HTTP_HOST'].'/schoolm/admin/';
+$GLOBALS['SITE_ADMIN_URL'] = 'http://' .$_SERVER['HTTP_HOST'].'/schoolManage/admin/';
 $GLOBALS['PAGE_BEFORE_LOGIN'] = array('Admin\Controller\Index\login','Admin\Controller\Index\index');
